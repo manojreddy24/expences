80d6bf1256ecbd296ade8ed1d21476aa3bdb443f
manojreddy24 <avulamanojreddy@gmail.com>
2023-05-24 01:15:33 -0500

    all

---
+++ app.py
@@ -0,0 +1,307 @@
+import hashlib
+from flask import *
+import pymysql
+from datetime import *
+import random
+import string
+import smtplib
+from email.mime.text import MIMEText
+app.permanent_session_lifetime=timedelta(minutes=10)
+app = Flask(__name__,template_folder="templates",static_folder="static")
+app.secret_key="manbigdat"
+# Connect to PHPMyAdmin cloud database
+connection = pymysql.connect(
+    host='sql9.freesqldatabase.com',
+    user='sql9618784',
+    password='ej2mLj1XvS',
+    db='sql9618784'
+
+)
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'itsmymail425@gmail.com'
+EMAIL_HOST_PASSWORD = 'llhattqaxodqhyjp'
+EMAIL_PORT = 587
+# @app.route('/dairy_login')
+def dairy_login():
+    try:
+        with connection.cursor() as cursor:
+            sql = "CREATE TABLE IF NOT EXISTS dairy_login (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(200) NOT NULL, password VARCHAR(100) NOT NULL, dairycode INT NOT NULL, email VARCHAR(100) NOT NULL UNIQUE, verification_code VARCHAR(255), verified BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY (id), INDEX(username), INDEX(email))"
+            cursor.execute(sql)
+            connection.commit()
+        return "Table created: dairy_login"
+    except pymysql.err.OperationalError as e:
+        return "Error creating table: " + str(e)
+
+# expences
+# @app.route('/expences', methods=['GET', 'POST'])
+def create_expense_table():
+    try:
+        with connection.cursor() as cursor:
+            sql = """CREATE TABLE IF NOT EXISTS expenses (id INT NOT NULL AUTO_INCREMENT,username VARCHAR(200) NOT NULL,email VARCHAR(100) NOT NULL, expense_date DATE,amount DECIMAL(10, 2) NOT NULL,category VARCHAR(100) NOT NULL,notes TEXT,timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (id),INDEX(username),INDEX(email),INDEX(TIMESTAMP),FOREIGN KEY (username) REFERENCES dairy_login(username),FOREIGN KEY (email) REFERENCES dairy_login(email))"""
+            cursor.execute(sql)
+            connection.commit()
+        return "Table created: expenses"
+    except pymysql.err.OperationalError as e:
+        return "Error creating table: " + str(e)
+
+
+# @app.route('/view_table')
+def view_table():
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM dairy_login"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return render_template('view_table.html', result=result)
+
+# @app.route('/drop')
+def drop():
+    try:
+        with connection.cursor() as cursor:
+            sql = "DROP TABLE IF EXISTS dairy_login"
+            cursor.execute(sql)
+            connection.commit()
+        return "Table dropped: dairy_login"
+    except pymysql.err.OperationalError as e:
+        return "Error dropping table: " + str(e)
+
+def generate_code():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+
+BLOCK_SIZE = 16
+def hash_password(password):
+    # Convert password to bytes and hash using SHA-256 algorithm
+    hashed = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return hashed
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        dairycode = request.form['dairycode']
+
+        code = generate_code()
+        email = request.form['email']
+
+        # Check if password and confirmation match
+        if password != confirm_password:
+            flash("Passwords do not match. Please try again.")
+            return redirect(url_for('register'))
+
+        # Hash the password
+        hashed = hash_password(password)
+        dairycode = hash_password(dairycode)
+        sq1 = "INSERT INTO dairy_login (username,password,email,dairycode) VALUES (%s,%s,%s,%s)"
+        val = (username, hashed, email,dairycode)
+        with connection.cursor() as cursor:
+            cursor.execute(sq1,val)
+            connection.commit()
+
+        send_verification_email(email, code)
+        # flash('An email with verification code has been sent to your email address.')
+
+
+        # Store user credentials in MySQL database
+        return redirect(url_for('verify', email=email))
+    return render_template('register.html')
+
+# Send email to user with verification code
+def send_verification_email(to_email, code):
+    message = f'Your verification code is: {code}'
+    msg = MIMEText(message)
+    msg['Subject'] = 'Verification code for your account'
+    msg['From'] = EMAIL_HOST_USER
+    msg['To'] = to_email
+    with connection.cursor() as cursor:
+        sql = "UPDATE dairy_login SET verification_code = %s WHERE email = %s"
+        up_code=hash_password(code)
+        val = (up_code, to_email)
+        cursor.execute(sql, val)
+        connection.commit()
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+        server.sendmail(EMAIL_HOST_USER, to_email, msg.as_string())
+
+
+
+
+# Verify user's email address
+@app.route('/verify/<email>', methods=['GET', 'POST'])
+def verify(email):
+    if request.method == 'POST':
+        code = request.form['code']
+        sq1 = "SELECT * FROM dairy_login WHERE email = %s AND verification_code = %s"
+        up_code=hash_password(code)
+        val = (email, up_code)
+        with connection.cursor() as cursor:
+            cursor.execute(sq1, val)
+            data = cursor.fetchone()
+            if data:
+                sq2 = "UPDATE dairy_login SET verified = %s WHERE email = %s"
+                val2 = (True, email)
+                cursor.execute(sq2, val2)
+                connection.commit()
+                flash('Your email address has been verified.')
+                return redirect(url_for('login'))
+            else:
+                flash('Invalid verification code. Please try again.')
+    return render_template('verify.html', email=email)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed = hash_password(password)
+        print("username",username)
+        print("password",hashed)
+        sql="select * from dairy_login where username='{}' and password='{}'".format(username,hashed)
+        print("sql",sql)
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            data = cursor.fetchone()
+            # print("data",data[4])
+            session['email'] = data[4]
+
+
+            if data:
+                session['username'] = username  # Store the username in the session
+
+
+
+                return redirect(url_for('index'))
+            else:
+                flash("Invalid username or password")
+                return redirect(url_for('login'))
+
+
+    return render_template('login.html')
+
+
+@app.route('/index')
+def index():
+    if 'username' in session:  # Check if the username is stored in the session
+        username = session['username']  # Retrieve the username from the session
+        return render_template('index.html', username=username)  # Pass the username to the template
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/expense_entry', methods=['GET', 'POST'])
+def expense_entry():
+    if 'username' in session:  # Check if the username is stored in the session
+        username = session['username']
+        email=session['email']
+        if request.method == 'POST':
+            category = request.form['category']
+            amount = float(request.form['amount'])
+            notes = request.form['notes']
+            timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            date = request.form['date']
+
+
+
+            # Insert the expense data into the expenses table
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO expenses (username,email, category, amount, timestamp, notes,expense_date ) VALUES (%s,%s,%s, %s, %s, %s,%s)"
+                cursor.execute(sql, (username,email, category, amount, timestamp, notes,date))
+                connection.commit()
+
+            return redirect(url_for('view_expenses'))
+
+        return render_template('expense_entry.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+@app.route('/expences_view', methods=['GET'])
+def view_expenses():
+    if 'username' in session:  # Check if the username is stored in the session
+
+        username = session['username']
+
+
+        # Retrieve the expenses from the database
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM expenses WHERE username = '{}' ORDER BY expense_date DESC".format(username)
+
+            cursor.execute(sql)
+            # print("cursor",sql)
+            expenses = cursor.fetchall()
+
+
+
+        return render_template('expences_view.html', expenses=expenses, username=username)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/edit_expense', methods=['GET', 'POST'])
+def edit_expense():
+    if 'username' in session:  # Check if the username is stored in the session
+        if request.method == 'POST':
+            expense_id = request.form['expense_id']
+
+            category = request.form['category']
+            amount = float(request.form['amount'])
+            date = request.form['date']
+            notes = request.form['notes']
+
+            with connection.cursor() as cursor:
+                sql = "UPDATE expenses SET category = %s, amount = %s, expense_date = %s, notes = %s WHERE id = %s"
+                cursor.execute(sql, (category, amount, date, notes, expense_id))
+                connection.commit()
+
+            return redirect('/expences_view')
+
+        else:
+            expense_id = request.args.get('id')
+
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM expenses WHERE id = %s"
+                cursor.execute(sql, (expense_id,))
+                expense = cursor.fetchone()
+
+            return render_template('edit_expense.html', expense=expense)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/delete_expense', methods=['GET'])
+def delete_expense():
+    if 'username' in session:  # Check if the username is stored in the session
+        if request.method == 'GET':
+            expense_id = request.args.get('id')
+
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM expenses WHERE id = %s"
+            cursor.execute(sql, (expense_id))
+            connection.commit()
+
+        return redirect('/expences_view')
+    else:
+        return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
